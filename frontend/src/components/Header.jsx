025f2c30938923fd4f264330cdc3b6d55e438ac0
Muhammad Kamran <134825430+Kami-1716@users.noreply.github.com>
2024-02-16 00:26:11 +0500

    Add navigation links to Header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='bg-slate-300 mb-[1px]'>
+      <header className='flex justify-between items-center max-w-6xl mx-auto bg-slate-300 shadow-sm'>
+        <div>
+          <Link to="/">
+            <h1 className='text-xl font-bold'>Home</h1>
+          </Link>
+        </div>
+        <div>
+          <ul className='flex gap-4 py-4'>
+            <Link to="/about">
+              <li className='font-semibold'>
+                About
+              </li>
+            </Link>
+            <Link to="/sign-up">
+              <li className='font-semibold'>
+                Sign Up
+              </li>
+            </Link>
+            <Link to="/profile">
+              <li className='font-semibold'>
+                Profile
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </header>
+    </div>
   )
 }
 
